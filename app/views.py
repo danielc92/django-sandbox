727ab695fb41c16c1571fb9da0556fc358a8afb6
Daniel Corcoran <danielc.92@outlook.com>
2019-05-14 09:53:15 +1000

    Modified create.html

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Dog, DogOwner
-
+from .forms import DogForm
 
 # Create your views here.
 def dog_list(request):
@@ -11,3 +11,21 @@ def dog_list(request):
                'data':data}
 
     return render(request, 'dog_list.html', context)
+
+
+def dog_create(request):
+
+    if request.method == "POST":
+        form = DogForm(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+            print(cd)
+            return 'Success'
+
+    else:
+        form = DogForm()
+
+    context = {'form': form, 'title': 'Create a dog form.'}
+
+    return render(request, 'create.html', context)
