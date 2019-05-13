b97e8cd743fcc3ce3eeede87222551561bac8e56
Daniel Corcoran <danielc.92@outlook.com>
2019-05-14 09:33:01 +1000

    Added a dog model and dogowner model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Dog(models.Model):
+    owner_id = models.ForeignKey(DogOwner, on_delete=models.CASCADE)
+    dog_name = models.CharField(max_length=100)
+
+class DogOwner(models.Model):
+    owner_name = models.CharField(max_length=100)
+    owner_address = models.CharField(max_length=200)
+    
