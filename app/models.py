54bb842682a17f250dc4a9052616dcb52d45dcfc
Daniel Corcoran <danielc.92@outlook.com>
2019-05-14 09:38:46 +1000

    Added str functions to models

--- app/models.py
+++ app/models.py
@@ -3,11 +3,16 @@ from django.db import models
 # Models based on assumption that one dog can be owned by one owner
 # and multiple dogs can be owned by one owner (one to many)
 
+class DogOwner(models.Model):
+    owner_name = models.CharField(max_length=100)
+    owner_address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.owner_name
+
 class Dog(models.Model):
     owner_id = models.ForeignKey(DogOwner, on_delete=models.CASCADE)
     dog_name = models.CharField(max_length=100)
 
-class DogOwner(models.Model):
-    owner_name = models.CharField(max_length=100)
-    owner_address = models.CharField(max_length=200)
-    
+    def __str__(self):
+        return self.dog_name
