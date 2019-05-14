1ca199fb87166e4b26151cdff3c532412a3cb94b
Daniel Corcoran <danielc.92@outlook.com>
2019-05-14 12:47:33 +1000

    Added Tag and Article Model

--- app/models.py
+++ app/models.py
@@ -13,8 +13,23 @@ class DogOwner(m.Model):
 class Dog(m.Model):
     owner_id = m.ForeignKey(DogOwner, on_delete=m.CASCADE)
     dog_name = m.CharField(max_length=100)
-    dog_image = m.ImageField(upload_to='dogs/%Y/%m/%d/', 
-                                 default='default.jpg')
+    dog_image = m.ImageField(upload_to='dogs/%Y/%m/%d/', default='default.jpg')
 
     def __str__(self):
         return self.dog_name
+
+class Tag(m.Model):
+    tag_name = m.CharField(max_length=50)
+
+    def __str__(self):
+        return self.tag_name
+
+class Article(m.Model):
+    article_name = m.CharField(max_length=100)
+    article_content = m.TextField(default="This article has no content yet..")
+    article_ct_date = m.DateTimeField(auto_now=True)
+
+    tags = m.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.article_name
